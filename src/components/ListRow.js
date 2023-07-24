bfbe3c6ce88a92df0558e37bef44a3e80369d8fa
hprieur <hprieur@enovacom.fr>
2023-07-24 11:01:34 +0200

    ListRow Component

---
+++ src/components/ListRow.js
@@ -0,0 +1,42 @@
+(function () {
+
+    class ListRow extends HTMLElement {
+        constructor() {
+            super();
+            this._shadow = this.attachShadow({ mode: 'closed' });
+            this._shadow.appendChild(template.content.cloneNode(true));
+        }
+
+        static get observedAttributes() { return ["selected"]; }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+            if ("selected" === name) {
+                const elm = this._shadow.querySelector("main");
+                if (null === newValue) {
+                    elm.removeAttribute("selected");
+                } else {
+                    elm.setAttribute("selected", "");
+                }
+            }
+        }
+    };
+
+    const template = document.createElement('template');
+    template.innerHTML = `
+        <style>
+            main {
+                padding: 0.5em 1em;
+                background-color: inherit;
+                cursor: pointer;
+            }
+            main[selected], main:hover {
+                background-color: var(--hover-color, rgba(0, 0, 0, 5%));
+            }
+        </style>
+        <main>
+            <slot></slot>
+        </main>
+    `;
+
+    window.customElements.define('list-row', ListRow);
+})();
