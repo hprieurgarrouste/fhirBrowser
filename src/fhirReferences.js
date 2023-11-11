d6dae9e5ff4219d644520649fc75a9c562cfc34e
hprieur <hprieur@enovacom.fr>
2023-11-11 18:20:13 +0100

    references: implement ListFilter

--- src/fhirReferences.js
+++ src/fhirReferences.js
@@ -3,6 +3,7 @@ import template from "./templates/fhirReferences.html";
 import "./components/ListItem.js";
 import "./components/ListRow.js";
 import "./components/RoundButton.js";
+import "./components/ListFilter.js";
 import { FhirService } from "./services/Fhir.js";
 
 class FhirReferences extends HTMLElement {
@@ -23,11 +24,20 @@ class FhirReferences extends HTMLElement {
                 location.hash = `#${item.dataset.target}?${item.dataset.search}=${this._resourceId}`;
             }
         });
+
         this._shadow.querySelector('side-panel').onClose = ((event) => {
             this.hidden = true;
             event.preventDefault();
             event.stopPropagation();
         }).bind(this);
+
+        this._shadow.querySelector('list-filter').addEventListener("filterChanged", ({ detail }) => {
+            const filter = detail.text.toLowerCase();
+            const list = this._shadow.getElementById('list');
+            list.childNodes.forEach(row => {
+                row.hidden = !(row.dataset.target.toLowerCase().includes(filter) || row.dataset.search.toLowerCase().includes(filter));
+            });
+        });
     }
 
     clear() {
@@ -39,6 +49,7 @@ class FhirReferences extends HTMLElement {
         this._resourceType = resourceType;
         this._resourceId = resourceId;
         this.clear();
+        this._shadow.querySelector('list-filter').clear();
         const list = this._shadow.getElementById('list');
         const references = FhirService.references(resourceType.type);
         if (references) {
