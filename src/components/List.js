648156c09135cdb498d58299ef8a5dc27e227ab6
hprieur <hprieur@enovacom.fr>
2023-11-18 11:47:18 +0100

    List component: Creation

---
+++ src/components/List.js
@@ -0,0 +1,48 @@
+import template from "./templates/List.html";
+
+import "./ListFilter.js";
+
+class List extends HTMLElement {
+    constructor() {
+        super();
+        this._shadow = this.attachShadow({ mode: 'closed' });
+        this._shadow.innerHTML = template;
+        this._list = null;
+        this._slot = null;
+        this._onFilter = null;
+    }
+
+    connectedCallback() {
+        this._list = this._shadow.getElementById('list');
+        this._slot = this._shadow.querySelector('slot');
+        this._filter = this._shadow.querySelector('list-filter');
+        this._filter.hidden = (this._slot.children.length <= 10);
+        this._slot.addEventListener('slotchange', () => {
+            this._filter.hidden = (('function' !== typeof this._onFilter) || this._slot.assignedNodes().length <= 10);
+        });
+        this._filter.onChange = ((value) => {
+            if (('function' == typeof this._onFilter)) {
+                if (value == '') this._list.scrollTop = 0;
+                this._onFilter(value);
+            }
+        });
+    }
+
+    clear() {
+        this._list.scrollTop = 0;
+        while (this._slot.firstChild) this._slot.removeChild(this._slot.lastChild);
+        this._filter.clear();
+    }
+
+    get onFilter() {
+        return this._onFilter;
+    }
+    set onFilter(filterFct) {
+        this._onFilter = filterFct;
+    }
+
+
+};
+
+customElements.define('app-list', List)
+
