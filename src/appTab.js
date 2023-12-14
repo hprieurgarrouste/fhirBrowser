c26f03ca9f0474b0678ba5e101692da788e5ddc1
hprieurgarrouste <h.prieurgarrouste@gmail.com>
2023-12-14 08:31:03 +0100

    tabs component: refactor

--- src/appTab.js
+++
@@ -1,9 +0,0 @@
-import template from "./templates/appTab.html";
-
-class AppTab extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'closed' }).innerHTML = template;
-    }
-};
-customElements.define('app-tab', AppTab);
