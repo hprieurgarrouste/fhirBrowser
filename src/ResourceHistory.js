a878997d894bdf08e164b977ac2e23246acb301f
hprieurgarrouste <h.prieurgarrouste@gmail.com>
2023-12-29 16:48:17 +0100

    history: refactor

--- src/ResourceHistory.js
+++ src/ResourceHistory.js
@@ -12,12 +12,15 @@ class ResourceHistory extends HTMLElement {
 
         this._shadow.getElementById('help').onclick = this.helpClick;
 
-        this._shadow.getElementById('list').onclick = this.listClick;
+        this._list = this._shadow.querySelector('app-list');
+        this._list.onclick = this.appListClick;
+
+        this._progress = this._shadow.querySelector('linear-progress');
 
         this._shadow.getElementById('close').onclick = this.sidePanelClose;
     }
 
-    listClick = (event) => {
+    appListClick = (event) => {
         event.preventDefault();
         event.stopPropagation();
         const item = event.target.closest("list-row");
@@ -44,51 +47,50 @@ class ResourceHistory extends HTMLElement {
         event.stopPropagation();
     }
 
-    clear() {
-        const list = this._shadow.getElementById('list');
-        while (list.firstChild) list.removeChild(list.lastChild);
-    }
-
     load(resourceType, resourceId) {
         if (!resourceType.interaction.find(({ code }) => 'vread' == code)) return;
         if (resourceType.type == this._resourceType && resourceId == this._resourceId) return
 
+        this._progress.hidden = false;
+        this._list.clear();
+
         FhirService.read(resourceType.type, resourceId).then(resource => {
-            const list = this._shadow.getElementById('list');
             this._resourceType = resourceType.type;
             this._resourceId = resource.id;
             const resourceVersionId = resource.meta?.versionId;
-            this.clear();
             FhirService.readHistory(resourceType.type, resource.id).then(response => {
-                if ('Bundle' == response.resourceType && 'history' == response.type && response.total > 1) {
-                    response.entry.sort((e1, e2) => {
-                        const d1 = new Date(e1.resource.meta.lastUpdated);
-                        const d2 = new Date(e2.resource.meta.lastUpdated);
-                        return d2 - d1;
-                    }).forEach(element => {
-                        const row = document.createElement('list-row');
-                        row.setAttribute("data-versionid", element.resource.meta.versionId);
-                        if (resourceVersionId == element.resource.meta.versionId) {
-                            row.setAttribute("selected", "");
-                        }
-                        const date = new Date(element.resource.meta.lastUpdated);
-                        const item = document.createElement('list-item');
-                        item.setAttribute("data-icon", "line_end_circle");
-                        item.setAttribute("data-primary", `${date.toLocaleString(undefined, {
-                            year: "numeric",
-                            month: "2-digit",
-                            day: "2-digit"
-                        })}`);
-                        item.setAttribute("data-secondary", `${date.toLocaleString(undefined, {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                            second: "2-digit",
-                            timeZoneName: "short"
-                        })}`);
-                        row.appendChild(item);
-                        list.appendChild(row);
-                    });
+                if ('Bundle' == response.resourceType && 'history' == response.type && response.total > 0) {
+                    response.entry
+                        .filter(element => element?.resource?.meta)
+                        .sort((e1, e2) => {
+                            const d1 = new Date(e1.resource.meta.lastUpdated);
+                            const d2 = new Date(e2.resource.meta.lastUpdated);
+                            return d2 - d1;
+                        }).forEach(element => {
+                            const row = document.createElement('list-row');
+                            row.setAttribute("data-versionid", element.resource.meta.versionId);
+                            if (resourceVersionId == element.resource.meta.versionId) {
+                                row.setAttribute("selected", "");
+                            }
+                            const date = new Date(element.resource.meta.lastUpdated);
+                            const item = document.createElement('list-item');
+                            item.setAttribute("data-icon", "history");
+                            item.setAttribute("data-primary", `${date.toLocaleString(undefined, {
+                                year: "numeric",
+                                month: "2-digit",
+                                day: "2-digit"
+                            })}`);
+                            item.setAttribute("data-secondary", `${date.toLocaleString(undefined, {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                                second: "2-digit",
+                                timeZoneName: "short"
+                            })}`);
+                            row.appendChild(item);
+                            this._list.appendChild(row);
+                        });
                 }
+                this._progress.hidden = true;
             });
         });
     }
